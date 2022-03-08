e82e577036317518e9b8b0768536c2a81dccad3b
Colin Laan <colin.laan@hva.nl>
2022-03-08 09:51:34 +0100

    Added roosterRoute.js

---
+++ pad-framework-master/server/routes/roosterRoute.js
@@ -0,0 +1,28 @@
+/**
+ * This is an example class to the route of a schedule
+ *
+ * @author Colin Laan
+ */
+
+class RoosterRoute {
+    #errorCodes = require("../framework/utils/httpErrorCodes")
+    #app
+
+    constructor(app) {
+        this.#app = app;
+
+        this.#getRoosterById()
+    }
+
+    #getRoosterById() {
+        this.#app.get("/rooster/:userId", async (req, res) => {
+            try {
+                res.status(this.#errorCodes.HTTP_OK_CODE).json("rooster van" + res.params.userId);
+            } catch(e) {
+                res.status(this.#errorCodes.BAD_REQUEST_CODE).json({reason: e})
+            }
+        });
+    }
+}
+
+module.exports = RoosterRoute
