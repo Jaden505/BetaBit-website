d28bc8fc3a4102bf6800590d54e4b79e3f413552
Colin Laan <colin.laan@hva.nl>
2022-03-15 00:44:33 +0100

    Added editDefaultSchedule styling, layout and partial functionality

--- pad-framework-master/src/assets/js/controllers/changeDefaultScheduleController.js
+++ pad-framework-master/src/assets/js/controllers/changeDefaultScheduleController.js
@@ -22,5 +22,30 @@ export class ChangeDefaultScheduleController extends Controller {
     async #setupView() {
         //await for when HTML is loaded
         this.#changeDefaultScheduleView = await super.loadHtmlIntoContent("html_views/changeDefaultSchedule.html")
+
+        this.#expandDayView()
+    }
+
+    #expandDayView() {
+        const expandTab = this.#changeDefaultScheduleView.getElementsByClassName("default-schedule-item")
+        const expandableContent = this.#changeDefaultScheduleView.getElementsByClassName("default-schedule-container-content")
+        const expandIcon = this.#changeDefaultScheduleView.getElementsByClassName("expand-img")
+
+        for (let i = 0; i < expandTab.length; i++) {
+            expandTab[i].addEventListener("click", function (){
+                expandableContent[i].classList.toggle("acc-active")
+                if (expandIcon[i].style.transform === "rotate(180deg)") {
+                    expandIcon[i].style.transform = "rotate(0)"
+                } else {
+                    expandIcon[i].style.transform = "rotate(180deg)"
+                }
+                let panel = expandableContent[i];
+                if (panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + "px";
+                }
+            })
+        }
     }
 }
