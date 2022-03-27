0f30a1c323c82b071ae5accfc990edc0b2a3b562
diaq <diaqfort@gmail.com>
2022-03-27 15:51:58 +0200

    Added currentMonth()

--- pad-framework-master/src/assets/js/controllers/individualMonthLeaderboardController.js
+++ pad-framework-master/src/assets/js/controllers/individualMonthLeaderboardController.js
@@ -8,11 +8,11 @@ import { Controller } from "./controller.js";
 
 export class IndividualMonthLeaderboardController extends Controller {
     #monthLeaderboardView
-    #today
+    today
 
     constructor() {
         super();
-        this.#today = new Date;
+        this.today = new Date;
 
         this.#setupView();
     }
@@ -26,20 +26,41 @@ export class IndividualMonthLeaderboardController extends Controller {
         //await for when HTML is loaded
         this.#monthLeaderboardView = await super.loadHtmlIntoContent("html_views/individualMonthLeaderboard.html");
 
-        this.#weekCounter();
+        this.#currentMonth();
+        this.weekCounter();
     }
 
-    #weekCounter() {
+    /**
+     * Gets the current month and displays it.
+     * @author Dia Fortmeier
+     * @memberOf IndividualMonthLeaderboardController
+     * @name currentMonth
+     * @function
+     * @private
+     * @instance
+     */
+    #currentMonth() {
+        let currentMonth = this.today.toLocaleString('default', { month: 'long' });
+        currentMonth = currentMonth.charAt(0).toUpperCase() + currentMonth.slice(1);
+        document.querySelector('#leaderboard-month').innerText = currentMonth;
+    }
+
+    /**
+     * Counter that checks and displays how long until Monday of next week
+     * @public
+     * @author Dennis Bleeker, Dia Fortmeier
+     */
+    weekCounter() {
         const countdown = () => {
             const second = 1000;
             const minute = second * 60;
             const hour = minute * 60;
             const day = hour * 24;
 
-            const nextMon = this.#today.getDate() - this.#today.getDay();
-            const countDate = new Date(this.#today.setDate(nextMon)).setHours(24,0,0,0);
-            const now = this.#today.getTime();
-            const gap = countDate - now;
+            this.today = new Date;
+            const nextMon = this.today.getDate() - this.today.getDay();
+            const countDate = new Date(this.today.setDate(nextMon)).setHours(24,0,0,0);
+            const gap = countDate - this.today.getTime();
 
             const textDay = Math.floor(gap / day);
             const textHour = Math.floor((gap % day) / hour);
